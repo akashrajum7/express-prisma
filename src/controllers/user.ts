a626b8c98e5ea2e71a915e3b697d0eb7d70925f6
akashrajum7 <akashrajum7@gmail.com>
2022-04-09 23:50:48 +0530

    add user update route

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -2,6 +2,7 @@ import { Response } from "express";
 import { asyncHandler } from "../middleware/async";
 import { SessionRequest } from "supertokens-node/framework/express";
 import { prisma } from "../utils/db";
+import { updateEmailOrPassword } from "supertokens-node/recipe/emailpassword";
 
 export const getCurrentUser = asyncHandler(
   async (req: SessionRequest, res: Response) => {
@@ -20,6 +21,74 @@ export const getCurrentUser = asyncHandler(
     res.status(200).send({
       user: {
         email: user.email,
+        bio: user.bio,
+        image: user.image,
+      },
+    });
+  }
+);
+
+export const updateCurrentUser = asyncHandler(
+  async (req: SessionRequest, res: Response) => {
+    const session = req.session;
+    if (!session) {
+      throw Error("Session not found");
+    }
+    const userId = session.getUserId();
+    if (!userId) {
+      throw Error("UserId not found");
+    }
+    let user = await prisma.user.findUnique({ where: { id: userId } });
+    if (!user) {
+      throw Error("User not found");
+    }
+
+    const { bio, image, email, password, username } = req.body;
+
+    let userDataToUpdateInDb: {
+      bio?: string;
+      image?: string;
+      username?: string;
+      email?: string;
+    } = {};
+
+    let userDataToUpdateInSupertokens: {
+      email?: string;
+      password?: string;
+      userContext?: any;
+      userId: string;
+    } = { userId: userId };
+
+    if (email) {
+      userDataToUpdateInSupertokens.email = email;
+      userDataToUpdateInDb.email = email;
+    }
+    if (password) {
+      userDataToUpdateInSupertokens.password = password;
+    }
+    if (bio) {
+      userDataToUpdateInDb.bio = bio;
+    }
+    if (image) {
+      userDataToUpdateInDb.image = image;
+    }
+    if (username) {
+      userDataToUpdateInDb.username = username;
+    }
+
+    if (email || password) {
+      await updateEmailOrPassword(userDataToUpdateInSupertokens);
+    }
+
+    user = await prisma.user.update({
+      where: { id: userId },
+      data: userDataToUpdateInDb,
+    });
+
+    res.status(200).send({
+      user: {
+        email: user.email,
+        username: user.username,
         bio: user.bio,
         image: user.image,
       },
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { verifySession } from "supertokens-node/recipe/session/framework/express";
-import { getCurrentUser } from "../controllers/user";
+import { getCurrentUser, updateCurrentUser } from "../controllers/user";
 
 const router = Router();
 
 router.get("/user", verifySession(), getCurrentUser);
+router.put("/user", verifySession(), updateCurrentUser);
 
 export default router;
