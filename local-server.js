ad82661a6a28b1062fea579d26288bd659cc4ae6
akashrajum7 <akashrajum7@gmail.com>
2022-04-10 16:28:39 +0530

    add netlify build

--- local-server.js
+++ local-server.js
@@ -1,6 +1,6 @@
-import app from "./index";
+import app from "./dist/index";
 
-const apiPort: string | number = process.env.PORT || 4000;
+const apiPort = process.env.PORT || 4000;
 
 app.listen(apiPort, () => {
   console.log(`Server is running on port ${apiPort}`);
